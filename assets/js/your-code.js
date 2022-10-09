1bfa850b467a658a1a30f7ca43a5f32c79714262
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-10-08 23:59:51 -0700

    get dogs

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,13 +1,17 @@
 export async function getAllDogs() {
   // Your code here
-  const url = '/';
+  const url = '/api/dogs';
   const dogs = await fetch(url);
   const res = await dogs.json();
   return res;
 }
 
-export function getDogNumberTwo() {
+export async function getDogNumberTwo() {
   // Your code here
+  const url = '/';
+  const dogs = await fetch(url);
+  const res = await dogs.json();
+  return res;
 }
 
 export function postNewDog() {
